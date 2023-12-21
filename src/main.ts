2b0f2d6538b1d5c2cd8532b90fe55aeb2c879c51
Jay Johnston <Jay.Johnston@Lynkz.com.au>
2023-12-21 20:06:22 +1100

    Refactor main.ts file to import and use separate settings files

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,26 @@
-import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-interface ConditionalTasksSettings {
-    mySetting: string;
-}
-
-const DEFAULT_SETTINGS: ConditionalTasksSettings = {
-    mySetting: 'default'
-}
+import { Plugin } from "obsidian";
+import { ConditionalTasksSettingTab } from "./Settings/ConditionalTasksSettingTab";
+import { ConditionalTasksSettings } from "./Settings/ConditionalTasksSettings";
+import { DEFAULT_SETTINGS } from "./Settings/DEFAULT_SETTINGS";
 
 export default class ConditionalTasksPlugin extends Plugin {
-    settings: ConditionalTasksSettings;
+	settings: ConditionalTasksSettings;
 
-    async onload() {
-        await this.loadSettings();
+	async onload() {
+		await this.loadSettings();
 
-        this.addSettingTab(new ConditionalTasksSettingTab(this.app, this));
-    }
+		this.addSettingTab(new ConditionalTasksSettingTab(this.app, this));
+	}
 
-    async loadSettings() {
-        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-    }
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
 
-    async saveSettings() {
-        await this.saveData(this.settings);
-    }
-}
-
-class ConditionalTasksSettingTab extends PluginSettingTab {
-    plugin: ConditionalTasksPlugin;
-
-    constructor(app: App, plugin: ConditionalTasksPlugin) {
-        super(app, plugin);
-        this.plugin = plugin;
-    }
-
-    display(): void {
-        const {containerEl} = this;
-
-        containerEl.empty();
-
-        new Setting(containerEl)
-            .setName('Conditional Tasks Setting')
-            .setDesc('This is a setting for the Conditional Tasks plugin')
-            .addText(text => text
-                .setPlaceholder('Enter your setting')
-                .setValue(this.plugin.settings.mySetting)
-                .onChange(async (value) => {
-                    this.plugin.settings.mySetting = value;
-                    await this.plugin.saveSettings();
-                }));
-    }
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
 }
